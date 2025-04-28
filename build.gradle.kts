9b250e9c1de8a60737fa35c507b2280f0eb8be83
Kevin Ho <79026373+cskevhho@users.noreply.github.com>
2025-04-27 20:26:14 -0400

    cli cmd option structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    // to change when testing different files through gradlew
+    mainClass.set("org.example.ArgParserKt")
+}
 
 repositories {
     mavenCentral()
@@ -11,6 +17,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.ajalt.clikt:clikt:5.0.3")
+    implementation("com.github.ajalt.clikt:clikt-markdown:5.0.3")
 }
 
 tasks.test {
--- src/main/kotlin/ArgParser.kt
+++ src/main/kotlin/ArgParser.kt
@@ -1,5 +1,30 @@
 package org.example
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.pair
 
-// logic: calls on kotlinx-cli to define arguments, maybe data class?
-class ArgParser {
+// thank you Clikt for building --help for me :')
+
+class Klex: CliktCommand(name = "klex") {
+    val since by option(help="Takes in <number><unit>, s,m,h,d -- default 1h")
+    val level by option(help="Takes in <level> -- <INFO><WARN><ERROR>")
+    val pattern by option(help="Takes in <regex>")
+    override fun run() = Unit
 }
+
+//class Hello: CliktCommand() {
+//    override fun run() {
+//        echo("Hello World")
+//    }
+//}
+//
+//class Tester: CliktCommand() {
+//   override fun run() {
+//        echo("This is a test run")
+//    }
+//}
+
+fun main(args: Array<String>) = Klex()
+    .main(args)
